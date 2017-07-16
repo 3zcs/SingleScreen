b8a22ca634ce3d2ce6be92af0e797fdede05fa24
3zcs <abdulaziz.aloshin@gmail.com>
2017-07-16 16:11:47 +0300

    Initial commit

---
+++ app/src/main/java/me/a3zcs/unforgettable/singlescreen/MainActivity.java
@@ -0,0 +1,13 @@
+package me.a3zcs.unforgettable.singlescreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
